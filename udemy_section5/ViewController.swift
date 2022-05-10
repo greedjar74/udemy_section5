9e0b25305f63151688e9e479aa4ef98f3b26e2a7
greedjar74 <hong7498kim@naver.com>
2022-05-10 11:38:47 +0900

    .

--- udemy_section5/ViewController.swift
+++ udemy_section5/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapAskButton(_ sender: UIButton) {
+        self.ballImageView.isHidden = false
+        self.ballImageView.image = UIImage(named: "ball\(Int.random(in: 1...5)).jpg")
+    }
+    
 
 }
 
